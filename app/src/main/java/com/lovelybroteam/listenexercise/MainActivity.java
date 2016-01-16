a7efe50a54c12a8326b6a458e0a84b38787717b8
Vo Quang Hoa <voquanghoa@gmail.com>
2016-01-16 22:47:35 +0700

    Finish share...

--- app/src/main/java/com/lovelybroteam/listenexercise/MainActivity.java
+++ app/src/main/java/com/lovelybroteam/listenexercise/MainActivity.java
@@ -1,12 +1,15 @@
 package com.lovelybroteam.listenexercise;
 
 import android.app.AlertDialog;
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 
 import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.plus.PlusShare;
 import com.lovelybroteam.listenexercise.constant.AppConstant;
 import com.lovelybroteam.listenexercise.control.BaseActivity;
 import com.lovelybroteam.listenexercise.controller.DataController;
@@ -27,11 +30,6 @@ public class MainActivity extends BaseActivity implements HttpDownloadController
                 finish();
             }
         });
-        findViewById(R.id.button_love_app).setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v) {
-                onLoveThisAppClick(v);
-            }
-        });
     }
 
     public void onBackPressed() {
@@ -46,7 +44,7 @@ public class MainActivity extends BaseActivity implements HttpDownloadController
     public void onLoveThisAppClick(View view){
         if(loveAppDialog==null){
             AlertDialog.Builder builder = new AlertDialog.Builder(this);
-            builder.setView (LayoutInflater.from(this).inflate(R.layout.love_app_dialog_layout, null));
+            builder.setView(LayoutInflater.from(this).inflate(R.layout.love_app_dialog_layout, null));
             loveAppDialog = builder.create();
         }
         loveAppDialog.show();
@@ -80,6 +78,48 @@ public class MainActivity extends BaseActivity implements HttpDownloadController
         } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
             Utils.Log(e);
+        }
+    }
+
+    public void onRateClick(View view){
+        String packageName = getPackageName();
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+
+        String marketLink = String.format(MARKET_URL_PATTERN, packageName);
+
+        intent.setData(Uri.parse(marketLink));
+        if (!SafeStartActivity(intent)) {
+            String marketWebLink = String.format(MARKET_WEB_URL_PATTERN, packageName);
+            intent.setData(Uri.parse(marketWebLink));
+            if (!SafeStartActivity(intent)) {
+                showMessage("Could not open Android market, please install the market app.");
+            }
+        }
+    }
+
+    public void onShareGPlusClick(View view){
+        String marketWebLink = String.format(MARKET_WEB_URL_PATTERN, getPackageName());
+        Intent shareIntent = new PlusShare.Builder(this)
+                .setType("text/plain")
+                .setText("Listen and exercise English for all level.")
+                .setContentUrl(Uri.parse(marketWebLink))
+                .getIntent();
+
+        SafeStartActivity(shareIntent);
+    }
+
+    public void onShareFacebookClick(View view){
+        String facebookShareLink = String.format(FACEBOOK_URL_PATTERN, getPackageName());
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(facebookShareLink));
+        SafeStartActivity(intent);
+    }
+
+    private boolean SafeStartActivity(Intent aIntent) {
+        try{
+            startActivity(aIntent);
+            return true;
+        }catch (ActivityNotFoundException e){
+            return false;
         }
     }
 }
--- app/src/main/java/com/lovelybroteam/listenexercise/constant/AppConstant.java
+++ app/src/main/java/com/lovelybroteam/listenexercise/constant/AppConstant.java
@@ -11,4 +11,8 @@ public interface AppConstant {
     String TEXT_FILE_EXTENSION = ".json";
     String AUDIO_FILE_EXTENSION = ".mp3";
     String PICTURE_FILE_EXTENSION = ".jpg";
+
+    String MARKET_URL_PATTERN = "market://details?id=%s";
+    String MARKET_WEB_URL_PATTERN = "https://play.google.com/store/apps/details?%s";
+    String FACEBOOK_URL_PATTERN = "https://www.facebook.com/sharer/sharer.php?u=%s";
 }
